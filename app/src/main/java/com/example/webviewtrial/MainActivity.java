d6842d3db0f2fd217463787c4a4dd725e2e76548
MarceloCLopes <marc01mcl@gmail.com>
2021-04-17 16:34:04 -0300

    accessing website via URL

--- app/src/main/java/com/example/webviewtrial/MainActivity.java
+++ app/src/main/java/com/example/webviewtrial/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.webviewtrial;
 
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.web_view);
+
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("http://www.google.com");
     }
 }
